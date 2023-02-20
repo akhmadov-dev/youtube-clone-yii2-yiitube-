a7d99fea8dff84560df84a036d4677e4718bd69c
ab <akhmadovdev@gmail.com>
2023-02-20 09:16:14 +0500

    yii tube

---
+++ common/models/Video.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%video}}".
+ *
+ * @property string $video_id
+ * @property string $title
+ * @property string|null $description
+ * @property string|null $tags
+ * @property int|null $status
+ * @property int|null $has_thumbnail
+ * @property string|null $video_name
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ *
+ * @property User $createdBy
+ */
+class Video extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%video}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['video_id', 'title'], 'required'],
+            [['video_id'], 'unique'],
+            [['video_id'], 'string', 'max' => 16],
+
+            [['description'], 'string'],
+            [['status', 'has_thumbnail', 'created_at', 'updated_at'], 'integer'],
+            [['title', 'tags', 'video_name'], 'string', 'max' => 512],
+
+            ['created_by', 'integer'],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'video_id' => Yii::t('backend', 'Video ID'),
+            'title' => Yii::t('backend', 'Title'),
+            'description' => Yii::t('backend', 'Description'),
+            'tags' => Yii::t('backend', 'Tags'),
+            'status' => Yii::t('backend', 'Status'),
+            'has_thumbnail' => Yii::t('backend', 'Has Thumbnail'),
+            'video_name' => Yii::t('backend', 'Video Name'),
+            'created_at' => Yii::t('backend', 'Created At'),
+            'updated_at' => Yii::t('backend', 'Updated At'),
+            'created_by' => Yii::t('backend', 'Created By'),
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\VideoQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\VideoQuery(get_called_class());
+    }
+}

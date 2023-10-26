add6935861848c8bbe8576e9af197cf4b8341872
Akhmadov <akhmadovdev@gmail.com>
2023-10-26 09:20:42 +0500

    implement dashboard

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -3,6 +3,9 @@
 namespace backend\controllers;
 
 use common\models\LoginForm;
+use common\models\Subscribe;
+use common\models\Video;
+use common\models\VideoView;
 use Yii;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
@@ -62,7 +65,34 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $user = Yii::$app->user->identity;
+        $userId = $user->id;
+
+        $latestVideo = Video::find()
+            ->latest()
+            ->creator($userId)
+            ->limit(1)
+            ->one();
+
+        $numberOfView = VideoView::find()
+            ->alias('vv')
+            ->innerJoin(Video::tableName() . ' v',
+                'v.video_id = vv.video_id')
+            ->andWhere(['v.created_by' => $userId])
+            ->count();
+
+        $numberOfSubscribers = $user->getSubscribers()->count();
+        $subscribers = Subscribe::find()->andWhere(['channel_id' => $userId])
+            ->orderBy('create_at DESC')
+            ->limit(3)
+            ->all();
+
+        return $this->render('index', [
+            'latestVideo' => $latestVideo,
+            'numberOfView' => $numberOfView,
+            'numberOfSubscribers' => $numberOfSubscribers,
+            'subscribers' => $subscribers
+        ]);
     }
 
     /**
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,10 +1,56 @@
 <?php
 
 /** @var yii\web\View $this */
+/** @var $latestVideo \common\models\Video */
+/** @var $numberOfView integer */
+/** @var $numberOfSubscribers integer */
+/** @var $subscribers \common\models\Subscribe[] */
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-
-   Index
+<div class="site-index d-flex">
+    <div class="card m-2" style="width: 18rem;">
+        <div class="ratio ratio-16x9 mb-3">
+            <video class="embed-responsive"
+                   poster="<?= $latestVideo->getThumbnailLink() ?>"
+                   src="<?= $latestVideo->getVideoLink() ?>"
+                   title="YouTube video"></video>
+        </div>
+        <div class="card-body">
+            <h6 class="card-title"><?= $latestVideo->title ?></h6>
+            <p class="card-text">
+                Likes: <?= $latestVideo->getLikes()->count() ?> <br>
+                Views: <?= $latestVideo->getViews()->count() ?>
+            </p>
+            <a href="<?= \yii\helpers\Url::to(['/video/update',
+                'id' => $latestVideo->video_id]) ?>"
+               class="btn btn-primary">Edit</a>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 18rem;">
+        <div class="card-body">
+            <h6 class="card-title">Total Views</h6>
+            <p class="card-text" style="font-size: 48px">
+               <?= $numberOfView ?>
+            </p>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 18rem;">
+        <div class="card-body">
+            <h6 class="card-title">Total Subscribers</h6>
+            <p class="card-text" style="font-size: 48px">
+               <?= $numberOfSubscribers ?>
+            </p>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 18rem;">
+        <div class="card-body">
+            <h6 class="card-title">Latest Subscribers</h6>
+           <?php foreach ($subscribers as $subscriber): ?>
+                <div>
+                    <?= $subscriber->user->username ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>

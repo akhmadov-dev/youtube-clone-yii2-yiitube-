894011de46157a32149c0d748a62371f58cea058
Akhmadov <akhmadovdev@gmail.com>
2023-10-26 14:19:04 +0500

    generate comment model

---
+++ common/models/Comment.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%comment}}".
+ *
+ * @property int $id
+ * @property string $comment
+ * @property string $video_id
+ * @property int|null $parent_id
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ *
+ * @property Comment[] $comments
+ * @property User $createdBy
+ * @property Comment $parent
+ * @property Video $video
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%comment}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['comment', 'video_id'], 'required'],
+            [['comment'], 'string'],
+            [['parent_id', 'created_at', 'updated_at', 'created_by'], 'integer'],
+            [['video_id'], 'string', 'max' => 16],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+            [['parent_id'], 'exist', 'skipOnError' => true, 'targetClass' => Comment::class, 'targetAttribute' => ['parent_id' => 'id']],
+            [['video_id'], 'exist', 'skipOnError' => true, 'targetClass' => Video::class, 'targetAttribute' => ['video_id' => 'video_id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('common', 'ID'),
+            'comment' => Yii::t('common', 'Comment'),
+            'video_id' => Yii::t('common', 'Video ID'),
+            'parent_id' => Yii::t('common', 'Parent ID'),
+            'created_at' => Yii::t('common', 'Created At'),
+            'updated_at' => Yii::t('common', 'Updated At'),
+            'created_by' => Yii::t('common', 'Created By'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Comments]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CommentQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::class, ['parent_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[Parent]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CommentQuery
+     */
+    public function getParent()
+    {
+        return $this->hasOne(Comment::class, ['id' => 'parent_id']);
+    }
+
+    /**
+     * Gets query for [[Video]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\VideoQuery
+     */
+    public function getVideo()
+    {
+        return $this->hasOne(Video::class, ['video_id' => 'video_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\CommentQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\CommentQuery(get_called_class());
+    }
+}

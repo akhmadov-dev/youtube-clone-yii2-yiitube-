8b5023afd6db3b6c3cf997e6d1a376ec2cf93e60
Akhmadov <akhmadovdev@gmail.com>
2023-10-28 09:36:28 +0500

    comment action menyu ui

--- frontend/views/video/_comment_item.php
+++ frontend/views/video/_comment_item.php
@@ -5,7 +5,7 @@
 
 ?>
 
-<div class="d-flex mb-3">
+<div class="d-flex mb-3 comment-item">
     <div class="flex-shrink-0">
         <img class="mr-3 comment-avatar" src="/image/avatar.svg" alt="Avatar Image">
     </div>
@@ -16,8 +16,30 @@
         </h6>
         <div class="comment-text">
             <?= $model->comment ?>
+            <div class="dropdown comment-actions">
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-thumbtack"></i> Pin
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-edit"></i> Edit
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-trash"></i> Delete
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="comment-actions mt-2">
+        <div class="button-action mt-2">
             <button class="btn btn-sm btn-light">
                 REPLY
             </button>
